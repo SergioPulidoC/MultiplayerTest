e3e0e81ce02774b253549e14ca28de8184741c1f
Sergio Pulido <sergio.pulido3@gmail.com>
2025-02-24 21:05:22 -0500

    Removed unnecessary comments and debug prints

--- Scripts/Network/MultiplayerManager.gd
+++ Scripts/Network/MultiplayerManager.gd
@@ -31,9 +31,9 @@ func StartClient(ip, port):
 func OnPeerConnected(peerId):
 	SpawnPlayer(peerId)
 	for existingPeerId in multiplayer.get_peers():
-		if existingPeerId != peerId:  # Avoid telling the new peer about itself
-			rpc_id(peerId, "RemoteSpawnPlayer", existingPeerId)  # Send existing players to the new peer
-	rpc("RemoteSpawnPlayer", peerId)  # Inform all clients to spawn the new player
+		if existingPeerId != peerId:
+			rpc_id(peerId, "RemoteSpawnPlayer", existingPeerId)
+	rpc("RemoteSpawnPlayer", peerId)
 
 @rpc("any_peer")
 func RemoteSpawnPlayer(peerId):
@@ -54,9 +54,3 @@ func SpawnPlayer(peerId):
 	player.global_transform.origin = availableSeat.spawnPoint.global_transform.origin
 	player.global_transform.basis = availableSeat.spawnPoint.global_transform.basis
 	availableSeat.occupier = player
-
-	## Debug authority information
-	#print("Spawned player ", peerId, " at ", availableSeat.name)
-	#print("Player spawned on peer ", multiplayer.get_unique_id())
-	#print("Is this the server? ", multiplayer.is_server())
-	#print("Player authority assigned to: ", player.get_multiplayer_authority())
